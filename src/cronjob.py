39cb27249724c2a1ae583c4d3a97c42938c80524
Avinash Mishra <avinash.mishra@tum.de>
2016-05-05 12:00:52 +0200

    added cronjob code

---
+++ src/cronjob.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+Created on Thu Apr 28 15:49:19 2016
+
+@author: rajababu
+"""
+ 
+target=open('/home/rajababu/Desktop/output.txt', 'a')
+target.write("function called")
+target.write("\n")
+target.close()
+ 
+   
