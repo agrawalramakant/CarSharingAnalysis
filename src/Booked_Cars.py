4663fa87ed9751379593d02a79648c0184aa8e9e
Avinash Mishra <avinash.mishra@tum.de>
2016-05-20 01:08:08 +0200

    Data insertion in database is working.

--- src/Booked_Cars.py
+++ src/Booked_Cars.py
@@ -6,28 +6,30 @@ Created on Thu May 19 11:35:27 2016
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float
-import itertools
 
 global Base 
 Base = declarative_base()
 class Booked_Cars(Base):
     
     def __init__(self,json_file,time):
-        self.record_id=itertools.count().next
+        
         self.Car_lic_No=json_file['vhc'][0]['lic']
         self.Location_X=json_file['loc'][0]
         self.Location_Y=json_file['loc'][0]
-        self.Date=time[0:9]
+        self.Date=time[0:8]
         self.Time=time[9:15]
         self.Car_provider=json_file['id'][0:2]
         self.Car_id=json_file['id']
         self.Car_type=json_file['vhc'][0]['typ']
-        self.Zone_id=json_file['zid']
+        if 'zid' in json_file:
+            self.Zone_id=json_file['zid']
+        else:
+            self.Zone_id=0
         self.Car_model=json_file['vhc'][0]['mdl']
     
     __tablename__='Booked_Cars'
     
-    record_id=Column(Integer, primary_key=True)
+    record_id=Column(Integer, primary_key=True,autoincrement=True)
     Car_lic_No= Column(String)
     Location_X= Column(Float)
     Location_Y= Column(Float)
@@ -39,6 +41,6 @@ class Booked_Cars(Base):
     Zone_id	= Column(Integer)
     Car_model= Column(String)
     
-    def __repr__(self):
-         return "<Car_daily_data(Car_lic_No='%s', Location_X='%s', Location_Y='%s',Date='%d',Time='%s',Car_provider= '%s',Car_id='%d' , Car_type= '%s',Zone_id='%d',Car_model= '%s',)>" % (self.Car_lic_No, self.Location_X, self.Location_Y, self.Date , self.Time , self.Car_provider ,  self.Car_id , self.Car_type, self.Zone_id , self.Car_model)
+   # def __repr__(self):
+        # return "<Car_daily_data(Car_lic_No='%s', Location_X='%s', Location_Y='%s',Date='%d',Time='%s',Car_provider= '%s',Car_id='%d' , Car_type= '%s',Zone_id='%d',Car_model= '%s',)>" % (self.Car_lic_No, self.Location_X, self.Location_Y, self.Date , self.Time , self.Car_provider ,  self.Car_id , self.Car_type, self.Zone_id , self.Car_model)
          
--- src/Work.py
+++ src/Work.py
@@ -8,14 +8,12 @@ Created on Thu May 19 12:09:18 2016
 from Booked_Cars import Booked_Cars 
 #import Released_Cars
 from DB_Entry import DB_Entry
-import json
+#import json
+import yaml
 
-#def __init__(self):
 
-with open ("Sample_data.json", "r") as f:
-    data = json.load(f) 
-#json_data1=open("Sample_data.json").read()
-#json_file=json.loads(json_data1)
+with open ("sample_data.json", "r") as f:
+    data = yaml.safe_load(f) 
 t=Booked_Cars(data,'16_05_19_16_20')
 ab = DB_Entry()
 ab.Add_entry(t)
