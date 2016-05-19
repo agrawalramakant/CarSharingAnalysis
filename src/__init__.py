7dd13a6bb7156eedf24fad633bafee47884d5e17
Ramakant Agrawal <ramakantagrawal@gmail.com>
2016-05-19 10:17:21 +0200

    last weeks commit

--- src/__init__.py
+++ src/__init__.py
@@ -4,11 +4,23 @@ Created on 05.05.2016
 @author: r.agrawal
 '''
 from datacollector import DataCollector
+import json
+import os.path
+import datetime
+
+tempFile = "temp.txt"
+
+def fetchAndSaveData():
+    datacollector = DataCollector()
+    time = datetime.datetime.now()
+    data = datacollector.getCarsData()
+    print data
+    if(os.path.exists(tempFile)):
+        missingData = json.dumps(datacollector.getMissingCars(data,tempFile))
+        print missingData
+    datacollector.writeToFile(data, tempFile)
+#   write to data base
 
 if __name__ == '__main__':
-    datacollector = DataCollector()
-    data = datacollector.getCarsData()
-    datacollector.writeToFile(data)
-#     newData = datacollector.loadOldData('data.txt')
-    datacollector.getMissingCars(data)
     
+    fetchAndSaveData()
--- src/datamodifier.py
+++ src/datamodifier.py
@@ -4,6 +4,6 @@ Created on 05.05.2016
 @author: r.agrawal
 '''
 import json
-class Modifier:
+class Modifier(object):
     
         
