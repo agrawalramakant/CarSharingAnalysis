1b85cd5dc6b00377185ce07d5e242e14ba73dff9
Avinash Mishra <avinash.mishra@tum.de>
2016-06-16 11:03:43 +0200

    test change

--- src/Work.py
+++ src/Work.py
@@ -7,7 +7,7 @@ Created on Thu May 19 12:09:18 2016
 
 from Booked_Cars import Booked_Cars 
 #import Released_Cars
-from DB_Entry import DB_Entry
+#from DB_Entry import DB_Entry
 #import json
 import yaml
 import json
@@ -17,5 +17,5 @@ with open ("temp.txt", "r") as f:
     data = json.load(f) 
 list = list(data)
 t=Booked_Cars(list[3],'2016_05_19_16_20')
-ab = DB_Entry()
-ab.Add_entry(t)
+#ab = DB_Entry()
+#ab.Add_entry(t)
---
+++ src/load_data.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 12 21:17:27 2016
+
+@author: avinashchandra
+"""
+
+import load_data_class
+import sqlalchemy as sqal
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.orm import sessionmaker
+
