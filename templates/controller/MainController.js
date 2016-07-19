4b08176c0b86d8e9f432c987401b29260842e017
Avinash Mishra <avinash.mishra@tum.de>
2016-07-19 16:41:08 +0200

    added garching1 and garching2 in the grid added popup feature to the markers. made the probability scale to 1 instead of 100

--- templates/controller/MainController.js
+++ templates/controller/MainController.js
@@ -48,6 +48,14 @@ app.controller("myCtrl", function($scope, service,$rootScope,$location,$window) 
     var max_airport_lng = 11.82077;
     var min_airport_lat = 48.337745;
     var min_airport_lng = 11.744986;
+    var max_garching1_lat = 48.271000;
+    var max_garching1_lng = 11.680500;
+    var min_garching1_lat = 48.258949;
+    var min_garching1_lng = 11.661000;
+    var max_garching2_lat = 48.260631;
+    var max_garching2_lng = 11.638151;
+    var min_garching2_lat = 48.243044;
+    var min_garching2_lng = 11.601713;
 
     var lat_factor = 0.00360;
     var lng_factor = 0.00535;
@@ -220,11 +228,14 @@ app.controller("myCtrl", function($scope, service,$rootScope,$location,$window) 
                     var node = $scope.prob_input[key];
                     if (node['prob'] != 0) {
                         var marker = L.marker([node['lat'], node['lng']],{color:getColor(node['prob']*100/max_prob)});
+                        marker.bindPopup("Probability to zone "+key +" is "+node['prob']).openPopup();
                         list_marker.push(marker);
                     }
                 }
             }
-            list_marker.push(L.marker([source_lat,source_lng],{color:'black'}));
+            var source_marker = L.marker([source_lat,source_lng],{color:'black'});
+            source_marker.bindPopup("Source Zone :"+sourceZone);
+            list_marker.push(source_marker);
             prob_marker_layer = L.featureGroup(list_marker).addTo(map);
             prob_info = L.control();
 
@@ -343,6 +354,66 @@ app.controller("myCtrl", function($scope, service,$rootScope,$location,$window) 
                     }
                 });
             idx++;
+            newZone = getCZone(max_garching1_lat, max_garching1_lng, min_garching1_lat, min_garching1_lng);
+
+            zones_dict[idx] = newZone;
+            for (z=0;z<input.length;z++){
+                var lat_orig=input[z]['lat'];
+                var lng_orig=input[z]['lng'];
+                if(lat_orig>newZone['min_lat'] && lat_orig<=newZone['max_lat'] && lng_orig>newZone['min_lng'] && lng_orig<=newZone['max_lng']){
+                    input[z]['zid'] = idx;
+                    newZone['count'] = newZone['count'] + 1;
+                    console.log(x+'is the selected zone');
+                }
+            }
+
+            list_rectangles.push(
+                {
+                    "type": "Feature",
+                    "properties": {'zid':idx, 'count':newZone['count']},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [[
+                            [newZone['max_lng'], newZone['max_lat']],
+                            [newZone['max_lng'], newZone['min_lat']],
+                            [newZone['min_lng'], newZone['min_lat']],
+                            [newZone['min_lng'], newZone['max_lat']]
+
+                        ]]
+                    }
+                });
+            idx++;
+
+
+            // Garching 2
+            newZone = getCZone(max_garching2_lat, max_garching2_lng, min_garching2_lat, min_garching2_lng);
+
+            zones_dict[idx] = newZone;
+            for (z=0;z<input.length;z++){
+                var lat_orig=input[z]['lat'];
+                var lng_orig=input[z]['lng'];
+                if(lat_orig>newZone['min_lat'] && lat_orig<=newZone['max_lat'] && lng_orig>newZone['min_lng'] && lng_orig<=newZone['max_lng']){
+                    input[z]['zid'] = idx;
+                    newZone['count'] = newZone['count'] + 1;
+                    console.log(x+'is the selected zone');
+                }
+            }
+
+            list_rectangles.push(
+                {
+                    "type": "Feature",
+                    "properties": {'zid':idx, 'count':newZone['count']},
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [[
+                            [newZone['max_lng'], newZone['max_lat']],
+                            [newZone['max_lng'], newZone['min_lat']],
+                            [newZone['min_lng'], newZone['min_lat']],
+                            [newZone['min_lng'], newZone['max_lat']]
+
+                        ]]
+                    }
+                });
 
             // for the geoJSON
             var gridStyle = {
@@ -457,178 +528,8 @@ app.controller("myCtrl", function($scope, service,$rootScope,$location,$window) 
 
 
 
-
-
-
-    var max_lat = 48.20775;
-    zones_dict = {}
-    zone = {'min_lat':0,
-        'max_lat':0,
-        'min_lng':0,
-        'max_lng':0,
-        'count':0}
-    var grid_Line_Color = 'black';
-    var grid_weight = 0.5;
-
-
-
-    var input=[{'lat': 48.065283,'lng': 11.443477,'zid':-1},
-        {'lat': 48.067628,'lng':11.546044,'zid':-1 },
-        {'lat': 48.067241,'lng':11.547678,'zid':-1 },
-        {'lat': 48.06601,'lng':11.549174,'zid':-1 },
-        {'lat': 48.067271,'lng':11.547525,'zid':-1 }
-
-    ];
-    
-    var idx =0;
-    for(var x =0;x<no_Of_Blocks;x++){
-        for(var y=0;y<no_Of_Blocks;y++){
-            var tempZone = JSON.parse(JSON.stringify(zone))
-            tempZone['max_lat'] = min_city_lat + lat_factor*(x+1);
-            tempZone['max_lng'] = min_city_lng + lng_factor*(y+1);
-            tempZone['min_lat'] = min_city_lat + lat_factor *x;
-            tempZone['min_lng'] = min_city_lng + lng_factor *y;
-            tempZone['count'] = 0;
-            zones_dict[idx] = tempZone;
-
-            var bounds = [[tempZone['max_lat'], tempZone['max_lng']], [tempZone['min_lat'], tempZone['min_lng']]];
-            var rectangle=L.rectangle(bounds, {color: grid_Line_Color, weight: 1,fillOpacity: 0}).addTo(mymap);
-            idx++;
-        }
-    }
-    console.log(Object.keys(zones_dict).length);
-    for (var x=0;x<Object.keys(zones_dict).length;x++){
-        var cur_zone = zones_dict[x];
-        for (z=0;z<input.length;z++){
-            var lat_orig=input[z]['lat'];
-            var lng_orig=input[z]['lng'];
-            if(lat_orig>cur_zone['min_lat'] && lat_orig<=cur_zone['max_lat'] && lng_orig>cur_zone['min_lng'] && lng_orig<=cur_zone['max_lng']){
-                input[z]['zid'] = x;
-                cur_zone['count'] = cur_zone['count'] + 1;
-                console.log(x+'is the selected zone');
-            }
-        }
-    }
-    var no_of_HeatMap_Colors = 4;
-    var max_zone_count = 0;
-    for (var x=0;x<Object.keys(zones_dict).length;x++) {
-        if(max_zone_count < zones_dict[x]['count']){
-            max_zone_count = zones_dict[x]['count'];
-        }
-    }
-    color_list = ['green','yellow','orange','red'];
-
-
-    console.log(zones_dict);
-    console.log(input);
-
-    for(var x=0;x<input.length;x++){
-        zid = input[x]['zid']
-        var count = zones_dict[zid]['count']
-        percent = count * 100 /max_zone_count;
-        if(percent <25){
-            color = color_list[0];
-        }else if(percent <50){
-            color = color_list[1];
-        }else if(percent <75){
-            color = color_list[2];
-        }else{
-            color = color_list[3];
-        }
-        var marker = L.marker([input[x]['lat'], input[x]['lng']], {
-            color: color,
-            maxWidth: 10,
-//            fillColor: '#f03',
-            fillOpacity: 0.5
-        }).addTo(mymap);
-        marker.bindPopup("yes i am a popup").openPopup();
-    }
-
-//    Airport
-    var max_airport_lat = 48.366514;
-    var max_airport_lng = 11.82077;
-    var min_airport_lat = 48.337745;
-    var min_airport_lng = 11.744986;
-    var tempZone = JSON.parse(JSON.stringify(zone))
-    tempZone['max_lat'] = max_airport_lat;
-    tempZone['max_lng'] = max_airport_lng;
-    tempZone['min_lat'] = min_airport_lat;
-    tempZone['min_lng'] = min_airport_lng;
-    tempZone['count'] = 0;
-    zones_dict[idx] = tempZone;
-    var bounds = [[tempZone['max_lat'], tempZone['max_lng']], [tempZone['min_lat'], tempZone['min_lng']]];
-    var rectangle=L.rectangle(bounds, {color: grid_Line_Color, weight: 1,fillOpacity: 0}).addTo(mymap);
-    idx++;
-    var tempZone = JSON.parse(JSON.stringify(zone))
-    tempZone['max_lat'] = min_city_lat + lat_factor*(x+1);
-    tempZone['max_lng'] = min_city_lng + lng_factor*(y+1);
-    tempZone['min_lat'] = min_city_lat + lat_factor *x;
-    tempZone['min_lng'] = min_city_lng + lng_factor *y;
-    tempZone['count'] = 0;
-    zones_dict[idx] = tempZone;
-    var bounds = [[tempZone['max_lat'], tempZone['max_lng']], [tempZone['min_lat'], tempZone['min_lng']]];
-    var rectangle=L.rectangle(bounds, {color: grid_Line_Color, weight: 1,fillOpacity: 0}).addTo(mymap);
-    // airport grid commented
-    // for (i=0;i<9;i++){
-    //     var car_coordinates = [
-    //         {lat: 48.366514-lat_factor*i, lng: 11.744986},
-    //         {lat: 48.366514-lat_factor*i, lng: 11.82077}
-    //
-    //     ];
-    //     var polygon = L.polygon([car_coordinates
-    //     ],{
-    //         color: '#cc99ff',
-    //         fillColor: '#ffffff',
-    //         fillOpacity: 0.3
-    //     }).addTo(mymap);
-    // }
-    // for (i=0;i<15;i++){
-    //     var car_coordinates = [
-    //         {lat: 48.366514, lng: 11.744986+0.00541*i},
-    //         {lat: 48.337745, lng: 11.744986+0.00541*i}
-    //
-    //     ];
-    //     var polygon = L.polygon([car_coordinates
-    //     ],{
-    //         color: '#cc99ff',
-    //         fillColor: '#ffffff',
-    //         fillOpacity: 0.3
-    //     }).addTo(mymap);
-    // };
-
-
 //Garching 1
 
-    var max_garching1_lat = 48.271000;
-    var max_garching1_lng = 11.680500;
-    var min_garching1_lat = 48.258949;
-    var min_garching1_lng = 11.661000;
-    var tempZone = JSON.parse(JSON.stringify(zone));
-    tempZone['max_lat'] = max_garching1_lat;
-    tempZone['max_lng'] = max_garching1_lng;
-    tempZone['min_lat'] = min_garching1_lat;
-    tempZone['min_lng'] = min_garching1_lng;
-    tempZone['count'] = 0;
-    zones_dict[idx] = tempZone;
-    var bounds = [[tempZone['max_lat'], tempZone['max_lng']], [tempZone['min_lat'], tempZone['min_lng']]];
-    var rectangle=L.rectangle(bounds, {color: grid_Line_Color, weight: 1,fillOpacity: 0}).addTo(mymap);
-
-    // Garching 2
-
-    var max_garching2_lat = 48.260631;
-    var max_garching2_lng = 11.638151;
-    var min_garching2_lat = 48.243044;
-    var min_garching2_lng = 11.601713;
-
-    var tempZone = JSON.parse(JSON.stringify(zone));
-    tempZone['max_lat'] = max_garching2_lat;
-    tempZone['max_lng'] = max_garching2_lng;
-    tempZone['min_lat'] = min_garching2_lat;
-    tempZone['min_lng'] = min_garching2_lng;
-    tempZone['count'] = 0;
-    zones_dict[idx] = tempZone;
-    var bounds = [[tempZone['max_lat'], tempZone['max_lng']], [tempZone['min_lat'], tempZone['min_lng']]];
-    var rectangle=L.rectangle(bounds, {color: grid_Line_Color, weight: 1,fillOpacity: 0}).addTo(mymap);
 
 
 });
